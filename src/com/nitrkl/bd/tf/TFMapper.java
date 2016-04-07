96841f8f2f12cdeab6db9edbafa1bc854e2d4a46
ved <ved@ubuntu.(none)>
2016-04-07 01:51:29 -0700

    filters non query terms and emmit (term,doc,tf*idf) in 2nd job

--- src/com/nitrkl/bd/tf/TFMapper.java
+++ src/com/nitrkl/bd/tf/TFMapper.java
@@ -4,25 +4,40 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
+import com.nitrkl.bd.query.Query;
+import com.nitrkl.bd.tfidf.DocumentFrequencyValue;
+
 public class TFMapper extends Mapper<Object, Text, TermDocumentKey, IntWritable> {
 	private final static IntWritable one = new IntWritable(1);
+	Query query;
+
+	@Override
+	protected void setup(Mapper<Object, Text, TermDocumentKey, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		query = new Query();
+	}
+
 	public void map(Object ikey, Text ivalue, Context context) throws IOException, InterruptedException {
 		StringTokenizer itr = new StringTokenizer(ivalue.toString());
 
 		FileSplit split = (FileSplit) context.getInputSplit();
-		String filename=split.getPath().toString();
-		if(filename.endsWith("~")){
+		String filename = split.getPath().toString();
+		if (filename.endsWith("~")) {
 			return;
 		}
 		String[] filenameArray = filename.split("/");
 		String document = filenameArray[filenameArray.length - 1];
-		
 		while (itr.hasMoreTokens()) {
-			context.write(new TermDocumentKey(new Text(document),new Text(itr.nextToken().toLowerCase())), one);
+			String token = itr.nextToken().toLowerCase();
+			if (!query.containsTerm(token)) {
+				continue;
+			}
+			context.write(new TermDocumentKey(new Text(document), new Text(token)), one);
 		}
 	}
 
--- src/com/nitrkl/bd/tfidf/TFIDFReducer.java
+++ src/com/nitrkl/bd/tfidf/TFIDFReducer.java
@@ -5,64 +5,61 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import com.nitrkl.bd.query.Query;
+import com.nitrkl.bd.tf.TermDocumentKey;
 
 public class TFIDFReducer extends Reducer<Text, DocumentFrequencyValue, Text, Text> {
 	public static int TOTAL_IP_DOCS = 0;
+	Query query;
+
+	@Override
+	protected void setup(Reducer<Text, DocumentFrequencyValue, Text, Text>.Context context)
+			throws IOException, InterruptedException {
+		query = new Query();
+	}
 
 	public void reduce(Text _key, Iterable<DocumentFrequencyValue> values, Context context)
 			throws IOException, InterruptedException {
 
-//		Set<String> documents = new HashSet<>();
+		// Set<String> documents = new HashSet<>();
 
 		ArrayList<DocumentFrequencyValue> documentList = new ArrayList<>();
-		
-		DocumentFrequencyValue queryTerm=null;
-		
+
 		// count document frequency
 		for (DocumentFrequencyValue val : values) {
 			String document = val.getDocument().toString();
 			String frequency = val.getFrequency().toString();
-			if (!document.equals("q")) {
-				documentList.add(new DocumentFrequencyValue(document, frequency));
-//				documents.add(val.getDocument().toString());
-			}else{
-				//it also exists in query.
-				queryTerm=new DocumentFrequencyValue(document, frequency);
-			}
+			documentList.add(new DocumentFrequencyValue(document, frequency));
 		}
-		
+
 		// count idf
 		double idf = Math.log10((double) TOTAL_IP_DOCS / documentList.size());
-		
-				
-		// count and emit tf*idf for query
-		double tfidf=0;
-		double qtfidf=0;
-		
-		if(queryTerm!=null){
-			qtfidf = Double.parseDouble(queryTerm.getFrequency().toString()) * idf;
-//			context.write(_key, new Text(queryTerm.getDocument() + "\t" + qtfidf));
-		}else{
-			//if not a query term
-			return;
-		}
+
+		// count for query
+		double qtfidf = 0;
+
+		qtfidf = query.getTerm(_key.toString()) * idf;
+		query.setTFIDF(_key.toString(), qtfidf);
+//		context.write(_key, new Text("q \t" + qtfidf));
 		
 		// count and emit tf*idf for documents
-//		for (DocumentFrequencyValue val : documentList) {
-//			tfidf = Double.parseDouble(val.getFrequency().toString()) * idf;
-//			context.write(_key, new Text(val.getDocument() + "\t" + tfidf));
-//		}
+		for (DocumentFrequencyValue val : documentList) {
+			double tfidf = Double.parseDouble(val.getFrequency().toString()) * idf;
+			context.write(_key, new Text(val.getDocument() + "\t" + tfidf));
+		}
 
 		// count and emit document_term(tf*idf)*query_term(tf*idf) for documents
-		for (DocumentFrequencyValue val : documentList) {
-			tfidf = Double.parseDouble(val.getFrequency().toString()) * idf;
-			context.write(_key, new Text(val.getDocument() + "\t" + (tfidf*qtfidf)));
-		}
-		
-		
-		
+		// for (DocumentFrequencyValue val : documentList) {
+		// tfidf = Double.parseDouble(val.getFrequency().toString()) * idf;
+		// context.write(_key, new Text(val.getDocument() + "\t" +
+		// (tfidf*qtfidf)));
+		// }
+
 		// emit df and idf of term
 		// context.write(_key, new Text(documents.size() + " " +idf));
 	}
